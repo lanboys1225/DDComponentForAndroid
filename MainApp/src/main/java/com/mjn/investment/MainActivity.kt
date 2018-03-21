269bb60dc846ec85b67f7048dabbfebf3650127c
lanbing <lanbing@baogongyoucai.com>
2018-03-21 11:53:36 +0800

    run success

--- MainApp/src/main/java/com/mjn/investment/MainActivity.kt
+++ MainApp/src/main/java/com/mjn/investment/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.mjn.investment
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import com.luojilab.component.componentlib.router.Router
+import com.mjn.libs.comm.service.HomeService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,25 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+
+        val router = Router.getInstance()
+        if (router.getService(HomeService::class.java!!.getSimpleName()) != null) {
+            val service = router.getService(HomeService::class.java!!.getSimpleName()) as HomeService
+            var fragment = service.homeFragment
+            var ft = supportFragmentManager.beginTransaction()
+            ft.add(R.id.tab_content, fragment).commitAllowingStateLoss()
+
         }
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
