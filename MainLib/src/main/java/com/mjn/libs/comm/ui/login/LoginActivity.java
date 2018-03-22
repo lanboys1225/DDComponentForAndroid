8b9875bdcb7573cfe8a85c2d101055db3c8b2aa9
lanbing <lanbing@baogongyoucai.com>
2018-03-22 11:35:51 +0800

    更新build gradle文件

--- MainLib/src/main/java/com/mjn/libs/comm/ui/login/LoginActivity.java
+++ MainLib/src/main/java/com/mjn/libs/comm/ui/login/LoginActivity.java
@@ -1,13 +1,14 @@
 package com.mjn.libs.comm.ui.login;
 
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bing.lan.comm.view.MyToolbar;
 import com.mjn.libs.R;
-import com.mjn.libs.R2;
 import com.mjn.libs.base.MainLibActivity;
-
-import butterknife.BindView;
 
 /**
  * @author 蓝兵
@@ -15,8 +16,20 @@ import butterknife.BindView;
 public class LoginActivity extends MainLibActivity<ILoginContract.ILoginPresenter>
         implements ILoginContract.ILoginView {
 
-    @BindView(R2.id.toolbar)
+    //@BindView(R.id.toolbar)
+    //@BindView(R2.id.toolbar)
     MyToolbar mToolbar;
+    private android.widget.LinearLayout mLoginContainer;
+    private android.widget.ImageView mImageView3;
+    private android.widget.EditText mLoginAccountEt;
+    private android.widget.ImageView mIvDeletephone;
+    private android.widget.LinearLayout mLinearLayout;
+    private android.widget.ImageView mImageView4;
+    private android.widget.EditText mLoginPasswordEt;
+    private android.widget.ImageView mIvDeletepwd;
+    private android.widget.TextView mLoginLoginBtn;
+    private android.widget.TextView mLoginRegistBtn;
+    private android.widget.TextView mLoginFindPwdBtn;
 
     @Override
     protected int getLayoutResId() {
@@ -33,13 +46,26 @@ public class LoginActivity extends MainLibActivity<ILoginContract.ILoginPresente
 
     @Override
     protected void initViewAndData(Intent intent) {
-        //mToolbar = findViewById(R.id.toolbar);
-
+        initView();
         setToolBar(mToolbar, "登录页面", true, 0);
     }
 
     @Override
     protected void readyStartPresenter() {
 
+    }
+
+    private void initView() {
+        mLoginContainer = (LinearLayout) findViewById(R.id.login_container);
+        mImageView3 = (ImageView) findViewById(R.id.imageView3);
+        mLoginAccountEt = (EditText) findViewById(R.id.login_accountEt);
+        mIvDeletephone = (ImageView) findViewById(R.id.iv_deletephone);
+        mLinearLayout = (LinearLayout) findViewById(R.id.linearLayout);
+        mImageView4 = (ImageView) findViewById(R.id.imageView4);
+        mLoginPasswordEt = (EditText) findViewById(R.id.login_passwordEt);
+        mIvDeletepwd = (ImageView) findViewById(R.id.iv_deletepwd);
+        mLoginLoginBtn = (TextView) findViewById(R.id.login_loginBtn);
+        mLoginRegistBtn = (TextView) findViewById(R.id.login_registBtn);
+        mLoginFindPwdBtn = (TextView) findViewById(R.id.login_findPwdBtn);
     }
 }
